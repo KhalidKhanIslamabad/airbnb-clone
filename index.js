6a9d3b3dc96b5ccbd29b9c3b4257f4d961ff65e1
KhalidKhanIslamabad <109690951+KhalidKhanIslamabad@users.noreply.github.com>
2023-08-20 18:55:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://airbnb-frontend-iota.vercel.app',
   credentials: true, // Enable sending cookies and headers with credentials
 };
 
