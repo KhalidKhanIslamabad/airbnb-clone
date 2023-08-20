31708c8ec084cda054b44e05a90f35eea02fe75d
KhalidKhanIslamabad <109690951+KhalidKhanIslamabad@users.noreply.github.com>
2023-08-20 17:24:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,27 +30,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-async function uploadToS3(path, originalFilename, mimetype) {
-  const client = new S3Client({
-    region: 'us-east-1',
-    credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    },
-  });
-  const parts = originalFilename.split('.');
-  const ext = parts[parts.length - 1];
-  const newFilename = Date.now() + '.' + ext;
-  await client.send(new PutObjectCommand({
-    Bucket: bucket,
-    Body: fs.readFileSync(path),
-    Key: newFilename,
-    ContentType: mimetype,
-    ACL: 'public-read',
-  }));
-  return `https://${bucket}.s3.amazonaws.com/${newFilename}`;
-}
-
 function getUserDataFromReq(req) {
   console.log("coockies===>>",req.cookies.token)
   return new Promise((resolve, reject) => {
